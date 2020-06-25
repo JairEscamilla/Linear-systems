9e1dd137b88573d386d3857016c5db8cadfc664d
Jair Escamilla <hernandez_yo14@outlook.es>
2020-06-24 20:38:08 -0500

    Intentando obtener la Transformada de Fourier

--- modulacion.m
+++ modulacion.m
@@ -1,29 +1,36 @@
 t = 0:0.00001:0.01;
-
-% Señal de frecuencia de 3.5 KHz
-Sm = 0.5*sin(3500*t);
-subplot(3, 1, 2);
+% Señal de frecuencia de 3.5 KHz (Tiempo)
+Sm = 0.5*sin(2*pi*3500*t);
+subplot(4, 1, 1);
 plot(t, Sm);
-xlabel("Tiempo");
+xlabel("Tiempo");   
 ylabel("Amplitud");
-title("Señal de 3.5 KHZ");
+title("Señal de 3.5 KHZ (Tiempo)");
 grid on;
 
+% Señal de frecuencia de 3.5 KHz (Frecuencia)
+Smf = fft(Sm);
+subplot(4, 1, 2);
+plot(t, abs(Smf));
+xlabel("Frecuencia");
+ylabel("Amplitud");
+title("Señal de frecuencia de 3.5 KHz");
 
-% Señal de frecuencia de 540 KHz
-St = sin(540000*t);
-subplot(3, 1, 1);
+
+% Señal de frecuencia de 540 KHz (Tiempo)
+St = sin(2*pi*540000*t);
+subplot(4, 1, 3);
 plot(t, St);
 xlabel("Tiempo");
 ylabel("Amplitud");
-title("Señal de 540 KHz");
+title("Señal de 540 KHz (Tiempo)");
 grid on;
 
-% Señal modulada
+% Señal modulada (Tiempo)
 Sam = St.*Sm;
-subplot(3, 1, 3);
+subplot(4, 1, 4);
 plot(t, Sam);
 xlabel("Tiempo");
 ylabel("Amplitud");
-title("Señal modulada");
+title("Señal modulada (Tiempo)");
 grid on;
